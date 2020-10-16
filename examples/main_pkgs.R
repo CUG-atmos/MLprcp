822c74e61e7d8682a21a4dac6120dd61728aeb86
Dongdong Kong <kongdd@users.noreply.github.com>
2020-10-16 13:10:58 +0800

    add fcm

--- examples/main_pkgs.R
+++ examples/main_pkgs.R
@@ -1,25 +1,26 @@
 # !/usr/bin/Rscript
 # 2020-10-15
 # Dongdong Kong ----------------------------------------------------------------
-# source('examles/main_pkgs.R')
+# source('examples/main_pkgs.R', encoding = "utf-8")
+suppressMessages({
+    library(magrittr)
+    library(purrr)
+    library(data.table)
+    library(plyr)
+    library(stringr)
+    library(foreach)
+    library(iterators)
 
-library(magrittr)
-library(purrr)
-library(data.table)
-library(plyr)
-library(stringr)
-library(foreach)
-library(iterators)
+    library(JuliaCall)
+    ## spatial
+    library(raster)
+    library(rgdal)
+    library(sp)
+    library(sf)
+    library(stars)
 
-library(JuliaCall)
-## spatial
-library(raster)
-library(rgdal)
-library(sp)
-library(sf)
-library(stars)
-
-# personal packages
-library(Ipaper)
-library(latticeGrob)
-library(rcolors)
+    # personal packages
+    library(Ipaper)
+    library(latticeGrob)
+    library(rcolors)
+})
